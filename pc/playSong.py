9a4d1ce1c09a1a32988725ca9161d8bec03e194f
iaakhter <iaakhter@colby.edu>
2016-01-18 10:43:20 -0500

    made it so that the program quits on pressing q

--- pc/playSong.py
+++ pc/playSong.py
@@ -26,41 +26,51 @@ def main():
 	readCount = 0
 	
 	change_rate = 1
-	while (True):
-		#read from a file and get the speed
-		if(readCount>=readWait):
-			f = open("speed.txt","r")
-			string_number = f.read()
-			print "string_number", string_number
-			if(is_number(string_number)==False):
-				continue
-			new_rate = float(string_number)
-			print "change_rate", change_rate
-
-			if(new_rate!=change_rate):
-				s.stop()
-				change_rate = int(new_rate)
-				#read the current framerate of the song
-				spf = wave.open('bgSong.wav', 'rb')
-				rate=spf.getframerate()
-				signal = spf.readframes(-1)
-				
-				#change the framerate of the song
-				wf = wave.open('bgSong.wav', 'wb')
-				wf.setnchannels(channels)
-				wf.setsampwidth(swidth)
-				wf.setframerate(rate+change_rate)
-				
-				#write the new wave file
-				wf.writeframes(signal)
-				wf.close()
-				s = pygame.mixer.Sound('bgSong.wav')
-				
-				#play it again
-				ch = s.play()
-			readCount = 0	
 	
-		readCount+=1
+	playSong = True
+	while (True):
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					playSong = False
+		
+		if playSong:
+			if(readCount>=readWait):
+				f = open("speed.txt","r")
+				string_number = f.read()
+				print "string_number", string_number
+				if(is_number(string_number)==False):
+					continue
+				new_rate = float(string_number)
+				print "change_rate", change_rate
+
+				if(new_rate!=change_rate):
+					s.stop()
+					change_rate = int(new_rate)
+					#read the current framerate of the song
+					spf = wave.open('bgSong.wav', 'rb')
+					rate=spf.getframerate()
+					signal = spf.readframes(-1)
+				
+					#change the framerate of the song
+					wf = wave.open('bgSong.wav', 'wb')
+					wf.setnchannels(channels)
+					wf.setsampwidth(swidth)
+					wf.setframerate(rate+change_rate)
+				
+					#write the new wave file
+					wf.writeframes(signal)
+					wf.close()
+					s = pygame.mixer.Sound('bgSong.wav')
+				
+					#play it again
+					ch = s.play()
+				readCount = 0	
+	
+			readCount+=1
+		
+		else:
+			return
 		
 		
 if __name__ == "__main__":
